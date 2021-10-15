ec16fa61893f131f875fba7227a0663430a717e8
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-15 17:16:08 -0400

    Add a new route to get upcoming reservations

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -23,6 +23,20 @@ module.exports = function(router, database) {
     });
   });
 
+  router.get('/reservations/upcoming', (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.error("💩");
+      return;      
+    }
+    database.getUpcomingReservations(userId)
+    .then(reservations => res.send({ reservations }))
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  })
+
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
     database.addProperty({...req.body, owner_id: userId})

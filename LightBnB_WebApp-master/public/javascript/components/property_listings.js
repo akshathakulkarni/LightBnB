0847dfbf14b3c7db2480055a1d290ed6ff90a6d5
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-17 12:15:20 -0400

    Add click handlers for review button and add review button

--- LightBnB_WebApp-master/public/javascript/components/property_listings.js
+++ LightBnB_WebApp-master/public/javascript/components/property_listings.js
@@ -40,18 +40,20 @@ $(() => {
       $('.delete-button').on('click', function() {
         const idData = $(this).attr('id').substring(16);
         deleteReservation(idData)
+        
         .then(() => console.log('Success!'))
-        .catch(err => console.error(err));        
+        .catch(err => console.error(error));   
+        $(this).closest('article').remove();     
       })
       $('.add-review-button').on('click', function() {
         const idData = $(this).attr('id').substring(11);
         views_manager.show("newReview", idData);
       })
     } else {
-      $('.reserve-button').on('click', function() {
+      $(document).on('click','.reserve-button', function() {
         const idData = $(this).attr('id').substring(17);
         views_manager.show('newReservation', idData);
-      })
+      });
       $('.review_details').on('click', function() {
         const idData = $(this).attr('id').substring(15);
         views_manager.show('showReviews', idData);
